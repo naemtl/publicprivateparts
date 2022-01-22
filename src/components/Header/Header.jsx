dd0bda90d6e3bb79046231f9af926a87c8052612
Matthew Stinis <mpstinis@gmail.com>
2022-01-21 23:18:52 -0500

    add hamburger styling to header navmenu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 
 import LanguageButton from "../LanguageButton/LanguageButton";
@@ -9,6 +10,8 @@ import './Header.scss'
 const Header = () => {
     const { t } = useTranslation()
 
+    const [menuOpen, setMenuOpen] = useState(false);
+
     const displayLanguageButtons = () => (
         Object.keys(supportedLanguages).map((lang) => (
             <LanguageButton supportedLanguages={supportedLanguages} language={lang} />
@@ -19,53 +22,48 @@ const Header = () => {
         <header className='header'>
             {displayLanguageButtons()}
             <button className='header__control--open'>Menu</button>
-            <nav class="navmenu navmenu--closed">
-                <div class="navmenu__top">
-                    <div class="navmenu__lang-container">
+            <nav className="navmenu navmenu--closed">
+                <div className="navmenu__top">
+                    <div className="navmenu__lang-container">
                         {displayLanguageButtons()}
                     </div>
-                    <div class="navmenu__exit">
-                        <svg width="35" height="35" viewBox="0 0 35 35"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <g fill="none" fill-rule="evenodd" stroke="#9D8B02">
-                                <path d="M0 35 35 0M0 0l35 35"></path>
-                            </g>
-                        </svg>
-                    </div>
+                    <button className="navmenu__button--exit" aria-label="Menu toggle button">
+                        <span className="navmenu__hamburger"></span>
+                    </button>
                 </div>
-                <ol class="navmenu__list">
-                    <li class="navmenu__list-item">
+                <ol className="navmenu__list">
+                    <li className="navmenu__list-item">
                         <a href="en-index.html">
-                            <span class="navmenu__list-item--numeral">01</span>
+                            <span className="navmenu__list-item--numeral">01</span>
                             Home
                         </a>
                     </li>
-                    <li class="navmenu__list-item">
+                    <li className="navmenu__list-item">
                         <a href="en-odm.html">
-                            <span class="navmenu__list-item--numeral">02</span>
+                            <span className="navmenu__list-item--numeral">02</span>
                             Public / Private Parts ou L’Origine du monde
                         </a>
                     </li>
-                    <li class="navmenu__list-item">
+                    <li className="navmenu__list-item">
                         <a href="en-bod.html">
-                            <span class="navmenu__list-item--numeral">03</span>
+                            <span className="navmenu__list-item--numeral">03</span>
                             Somatic Sex Education and Bodywork Sessions
                         </a>
                     </li>
-                    <li class="navmenu__list-item">
+                    <li className="navmenu__list-item">
                         <a href="en-interviews.html">
-                            <span class="navmenu__list-item--numeral">04</span>
+                            <span className="navmenu__list-item--numeral">04</span>
                             Documentary Interviews
                         </a>
                     </li>
-                    <li class="navmenu__list-item">
+                    <li className="navmenu__list-item">
                         <a href="en-about.html">
-                            <span class="navmenu__list-item--numeral">05</span>
+                            <span className="navmenu__list-item--numeral">05</span>
                             About
                         </a>
                     </li>
                 </ol>
-                <div class="navmenu__contact type-ogg-regular-19-italic">
+                <div className="navmenu__contact type-ogg-regular-19-italic">
                     Say hi ⟶
                     <a href="mailto:gerardxreyes@gmail.com">gerardxreyes@gmail.com</a>
                 </div>
