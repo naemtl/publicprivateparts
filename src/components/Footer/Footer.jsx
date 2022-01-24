00edc82e14e798e751cc9d3c52912d632bb26d59
Matthew Stinis <mpstinis@gmail.com>
2022-01-23 19:57:04 -0500

    add footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,8 +2,17 @@ import './Footer.scss'
 
 const Footer = () => {
     return (
-        <div className='footer'>
-
+        <div class="footer">
+            <div class="footer__title">Contact:</div>
+            <div class="footer__social--email"><a href="mailto:gerardxreyes@gmail.com">gerardxreyes@gmail.com</a></div>
+            <div class="footer__social--insta"><a href="https://instagram.com/gerardxreyes">Instagram</a></div>
+            <div class="footer__social--fb"><a href="https://www.facebook.com/GerardReyesArtist">Facebook</a></div>
+            <div class="footer__social--youtube"><a
+                href="https://www.youtube.com/channel/UCuOTqqnX4obf7HefTmQZyng">YouTube</a>
+            </div>
+            <div class="footer__social--vimeo"><a href="https://vimeo.com/gerardxreyes">Vimeo</a></div>
+            <div class="footer__social--linkedin"><a
+                href="https://www.linkedin.com/in/gerard-x-reyes-77952a120">LinkedIn</a></div>
         </div>
     );
 };
