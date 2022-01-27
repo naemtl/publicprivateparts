2418a47c4760899b3bb5bc657fdad7a361957c97
Matthew Stinis <mpstinis@gmail.com>
2022-01-27 16:07:16 -0500

    add spinner to loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -8,6 +8,7 @@ const Loader = () => {
     return (
         <div className='loader'>
             <img src={pppLogo} alt="Public Private Parts logo" />
+            <FaSpinner />
         </div>
     );
 };
