78aa81c5c828b50dc7fca25acf55725da82eae60
Matthew Stinis <mpstinis@gmail.com>
2022-01-30 20:50:56 -0500

    add bodywork translation from i18next json

--- src/pages/Bodywork/Bodywork.jsx
+++ src/pages/Bodywork/Bodywork.jsx
@@ -1,32 +1,41 @@
+import { Trans, useTranslation } from "react-i18next";
+
 import Footer from '../../components/Footer/Footer';
+
 import './Bodywork.scss'
 
-
 const Bodywork = () => {
+    const { t } = useTranslation('bodywork')
+
     return (
         <div className="bodywork">
-            <h1 className="bodywork__header">Somatic Sex Education and Bodywork Sessions</h1>
+            <h1 className="bodywork__header">
+                {t("page-title")}
+            </h1>
             <div className="bodywork__content">
-                <h2>Society’s Relationship To Pleasure</h2>
-                <p> We all set limits on the quantity and quality of pleasure we can embody. Limits to pleasure are ubiquitous
-                    in a culture that shames sexuality and fails to teach us how to honour and celebrate our erotic selves.
-                    Sexual wounding becomes embedded in the body’s tissues and responses. Somatic Sex Education and Bodywork can
-                    assist clients in expanding the pleasure that is possible. This expansion has physical, mental, emotional,
-                    and relational benefits. </p>
-                <p> In 2019, I graduated from the esteemed <a href="https://instituteofsomaticsexology.com">Institute of Somatic
-                    Sexology</a> in Australia to become a Certified Sexological Bodyworker. I warmly welcome people of all
-                    genders, races, cultures, religions, sexual orientations and relationship choices to work with me. </p>
-                <h2>What Is Somatic Sex Education?</h2>
-                <p> Sexological Bodyworkers teach embodied sex education and offer bodywork. We teach through body experiences
-                    designed to nurture, deepen or awaken the sensual self. Our teaching can include coaching in breathwork,
-                    body awareness, movement, boundary-setting (consent), communication, anatomy, massage, mindful masturbation,
-                    scar tissue remediation and other body-based practices. </p>
-                <p> Not all teachings include touch, but when requested touch is one-way (from practitioner to client) and is
-                    client-led. We touch in ways that assist clients in developing presence within the body, opening interior
-                    awareness, empowering choice and voice, and learning how the body can feel safer and more alive. Uniquely in
-                    the profession, we are trained to offer client-led genital or anal touch for the purposes of education,
-                    healing and pleasure. Sexological Bodyworkers are trauma-informed and remain clothed during sessions. </p>
-                <h2>15 Reasons People Might See a Sexological Bodyworker</h2>
+                <h2>
+                    {t("heading-society")}
+                </h2>
+                <p>
+                    {t("paragraph-society-1")}
+                </p>
+                <p>
+                    <Trans t={t} i18nKey={"paragraph-society-2"}>
+                        In 2019, I graduated from the esteemed <a href="https://instituteofsomaticsexology.com">Institute of Somatic Sexology</a> in Australia to become a Certified Sexological Bodyworker. I warmly welcome people of all genders, races, cultures, religions, sexual orientations and relationship choices to work with me.
+                    </Trans>
+                </p>
+                <h2>
+                    {t("heading-somatic")}
+                </h2>
+                <p>
+                    {t("paragraph-somatic-1")}
+                </p>
+                <p>
+                    {t("paragraph-somatic-2")}
+                </p>
+                <h2>
+                    {t("heading-reasons")}
+                </h2>
                 <ol>
                     <li>Accepting and loving one’s body, gender and/or sexuality</li>
                     <li>Reconnecting to the sensual/sexual self</li>
@@ -44,7 +53,9 @@ const Bodywork = () => {
                     <li>Understanding anal pleasure</li>
                     <li>Referred by a Therapist, Doctor, Sexologist, Massage Therapist</li>
                 </ol>
-                <h2>What happens in a session?</h2>
+                <h2>
+                    {t("heading-session")}
+                </h2>
                 <ol>
                     <li>
                         <span>Check-in</span> – You and Gerard discuss your intentions, interests, challenges and
@@ -71,56 +82,61 @@ const Bodywork = () => {
                     </li>
                 </ol>
                 <div>
-                    <h2>Message from Gerard</h2>
-                    <p>As a non-binary and queer person (he/she/they), it has taken me decades of work to feel comfortable in my
-                        skin. Over 20
-                        years of training in dance, somatics, yoga and meditation helped me discover pleasure in my body. But it
-                        wasn’t until my
-                        training in Sexological Bodywork that I was given the tools to truly listen to it. I use this awareness
-                        to make choices
-                        that have improved both my sexual and everyday life.
+                    <h2>
+                        {t("heading-message")}
+                    </h2>
+                    <p>
+                        {t("paragraph-message-1")}
                     </p>
-                    <p>In 2019, I became a Certified Sexological Bodyworker by completing a six-month course at the Australian <a href="https://instituteofsomaticsexology.com/">Institute of
-                        Somatic Sexology</a>, which was facilitated by master educators Deej Juventin and Uma Ayelet. In
-                        2020, I continued my
-                        studies via a mentorship with Ron Stuart and Pono Stuart – two Certified Sexological Bodyworkers who are
-                        associated with
-                        the Institute for the Study of Somatic Sex Education in Victoria, Canada.
+                    <p>
+                        <Trans t={t} i18nKey={"paragraph-message-2"}>
+                            In 2019, I became a Certified Sexological Bodyworker by completing a six-month course at the Australian <a href="https://instituteofsomaticsexology.com/">Institute of
+                                Somatic Sexology</a>, which was facilitated by master educators Deej Juventin and Uma Ayelet. In 2020, I continued my studies via a mentorship with Ron Stuart and Pono Stuart – two Certified Sexological Bodyworkers who are
+                            associated with the Institute for the Study of Somatic Sex Education in Victoria, Canada.
+                        </Trans>
                     </p>
-                    <p>I am interested in offering experiences that expand consciousness and create deeper connections between
-                        body, mind and
-                        spirit. I warmly welcome people of all genders, races, cultures, religions, sexual orientations and
-                        relationship choices
-                        to work with me.
+                    <p>
+                        {t("paragraph-message-3")}
                     </p>
                 </div>
-                <h2>Prices for individuals</h2>
-                <div>
-                    <div>1 session (90 min): <strong>$110</strong></div>
-                    <div>3 sessions (90 min each): <strong>$300</strong></div>
-                </div>
-                <p>*Prices are in Canadian dollars and include taxes.</p>
-                <p> I can accommodate couples/triads, shorter sessions, financial difficulty, etc. Write to tell me about your
-                    situation! </p>
+                <h2>
+                    {t("heading-pricing")}
+                </h2>
                 <div>
                     <div>
-                        <a href="mailto:gerardxreyes@gmail.com">Email Gerard</a>
+                        <Trans t={t} i18nKey={"text-pricing-1"} />
                     </div>
                     <div>
-                        <a href="https://calendly.com/gerardxreyes/10-min-consultation-call-appel">Book a free consultation
-                            call</a>
+                        <Trans t={t} i18nKey={"text-pricing-2"} />
+                    </div>
+                </div>
+                <p>
+                    {t('paragraph-pricing-1')}
+                </p>
+                <p>
+                    {t('paragraph-pricing-2')}
+                </p>
+                <div>
+                    <div>
+                        <a href="mailto:gerardxreyes@gmail.com">
+                            {t("anchor-contact")}
+                        </a>
+                    </div>
+                    <div>
+                        <a href="https://calendly.com/gerardxreyes/10-min-consultation-call-appel">
+                            {t("anchor-book")}
+                        </a>
                     </div>
                 </div>
                 <hr />
-                <p> NOTE: For in-person sessions, I follow the Government of Québec’s public health guidelines to reduce the
-                    transmission of COVID-19 (ie. wearing masks, hand washing, sanitizing surfaces, etc.) and ask that all
-                    clients abide by these guidelines.
+                <p>
+                    {t("paragraph-covid")}
                 </p>
                 <p>
-                    <em>Thank you to Sexological Bodyworker Caffyn Jesse for their contribution to this description.</em>
+                    <em>{t("paragraph-thanks")}</em>
                 </p>
                 <p>
-                    <em>&copy; 2014 ACSB. All rights reserved.</em>
+                    <em>&copy; 2014 ACSB. {t("paragraph-copyright")}</em>
                 </p>
             </div>
             <Footer />
