ceebdd76289c303c1e408854bf7f120e6a9dc4f9
Matthew Stinis <mpstinis@gmail.com>
2022-02-01 13:25:43 -0500

    add locales json for origine personnel en

--- src/pages/OrigineDuMonde/OriginePersonnel/OriginePersonnel.jsx
+++ src/pages/OrigineDuMonde/OriginePersonnel/OriginePersonnel.jsx
@@ -6,196 +6,195 @@ const OriginePersonnel = ({ t }) => {
     return (
         <div className="personnel">
             <div className="personnel__heading">
-                A Gerard X Reyes production
+                {t("heading-main")}
             </div>
             <div className="personnel__row">
-                <span>CO-PRODUCTION</span>
+                <span>{t("roles.co-prod")}</span>
                 <span></span>
-                <span>FESTIVAL TRANSAMÉRIQUES</span>
+                <span>Festival Transamériques</span>
             </div>
             <div className="personnel__row">
-                <span>Director, Choreographer Interviewer, Co-editor</span>
+                <span>{t("roles.director-choreog")}</span>
                 <span></span>
                 <span>Gerard X Reyes</span>
             </div>
             <div className="personnel__row">
-                <span>In collaboration with:</span>
+                <span>{t("heading-collab")}:</span>
             </div>
-            <div className="personnel__heading">Video Installation</div>
+            <div className="personnel__heading">{t("heading-video")}</div>
             <div className="personnel__row">
-                <span>Editor, Video dramaturgical advice</span>
+                <span>{t("roles.editor-video")}</span>
                 <span></span>
                 <span>Julie Favreau</span>
             </div>
             <div className="personnel__row">
-                <span>Video installation, Projection and Programming</span>
+                <span>{t("roles.video-install")}</span>
                 <span></span>
                 <span>Stéphane Gladyszewski</span>
             </div>
             <div className="personnel__row">
-                <span>Co-director</span>
+                <span>{t("roles.co-director")}</span>
                 <span></span>
                 <span>Poppy Sanchez</span>
             </div>
             <div className="personnel__row">
-                <span>Performers</span>
+                <span>{t("roles.performers")}</span>
                 <span></span>
                 <span>Gerard X Reyes, Bishop Black, Jasko Fide, JorgeTheObscene, Mara Morgen, Rebecca Jackson</span>
             </div>
             <div className="personnel__row">
-                <span>Director of photography</span>
+                <span>{t("roles.photography")}</span>
                 <span></span>
                 <span>Kevin Klein</span>
             </div>
             <div className="personnel__row">
-                <span>Second camera operator</span>
+                <span>{t("roles.second-camera")}</span>
                 <span></span>
                 <span>Jara Lopez Ballonga</span>
             </div>
             <div className="personnel__row">
-                <span>Sound recordist</span>
+                <span>{t("roles.sound-recordist")}</span>
                 <span></span>
                 <span>Antonio Vilchez Monge</span>
             </div>
             <div className="personnel__row">
-                <span>Set designer</span>
+                <span>{t("roles.set-designer")}</span>
                 <span></span>
                 <span>Jasmine Reimer</span>
             </div>
             <div className="personnel__row">
-                <span>Assistant set designer</span>
+                <span>{t("roles.assistant-set")}</span>
                 <span></span>
                 <span>Kaitlyn Smeeth</span>
             </div>
             <div className="personnel__row">
-                <span>Sound editor</span>
+                <span>{t("roles.sound-editor")}</span>
                 <span></span>
                 <span>Devon Bate</span>
             </div>
             <div className="personnel__row">
-                <span>Color correction</span>
+                <span>{t("roles.colour-correction")}</span>
                 <span></span>
                 <span>Max Hilsamer</span>
             </div>
             <div className="personnel__row">
-                <span>Subtitles</span>
+                <span>{t("roles.subtitles")}</span>
                 <span></span>
                 <span>T &amp; S Coop</span>
             </div>
             <div className="personnel__row">
-                <span>Subtitles Translation (French)</span>
+                <span>{t("roles.subtitles-fr")}</span>
                 <span></span>
                 <span>Ingrid Vallus</span>
             </div>
             <div className="personnel__row">
-                <span>Camera operator (interviews)</span>
+                <span>{t("roles.camera-interviews")}</span>
                 <span></span>
                 <span>David J. Romero</span>
             </div>
             <div className="personnel__row">
-                <span>Production Manager</span>
+                <span>{t("roles.vid-prod-manager")}</span>
                 <span></span>
                 <span>Elisa Hengen</span>
             </div>
-            <div className="personnel__heading">Live Performance</div>
+            <div className="personnel__heading">{t("heading-live-performance")}</div>
             <div className="personnel__row">
-                <span>Performers</span>
+                <span>{t("roles.live-performers")}</span>
                 <span></span>
                 <span>Gerard X Reyes, Justin Gionet, Emmanuel Proulx</span>
             </div>
             <div className="personnel__row">
-                <span>Movement research assistant</span>
+                <span>{t("roles.movement-research")}</span>
                 <span></span>
                 <span>Andréane Leclerc</span>
             </div>
             <div className="personnel__row">
-                <span>Dramaturgical advice</span>
+                <span>{t("roles.drama-advice")}</span>
                 <span></span>
                 <span>Guy Cools</span>
             </div>
             <div className="personnel__row">
-                <span>Lighting designer, Technical director</span>
+                <span>{t("roles.lighting-tech")}</span>
                 <span></span>
                 <span>Karine Gauthier</span>
             </div>
             <div className="personnel__row">
-                <span>Music</span>
+                <span>{t("roles.music")}</span>
                 <span></span>
                 <span>Bendik Giske</span>
             </div>
             <div className="personnel__row">
-                <span>Composition</span>
+                <span>{t("roles.composition")}</span>
                 <span></span>
                 <span>Devon Bate</span>
             </div>
             <div className="personnel__row">
-                <span>Spatialization</span>
+                <span>{t("roles.spatialization")}</span>
                 <span></span>
                 <span>Devon Bate, Dominic Jasmin</span>
             </div>
             <div className="personnel__row">
-                <span>Props designer</span>
+                <span>{t("roles.props-designer")}</span>
                 <span></span>
                 <span>Jasmine Reimer</span>
             </div>
             <div className="personnel__row">
-                <span>Vocal coach</span>
+                <span>{t("roles.vocal-coach")}</span>
                 <span></span>
                 <span>Ann Dyer</span>
             </div>
             <div className="personnel__row">
-                <span>Production managers</span>
+                <span>{t("roles.live-prod-manager")}</span>
                 <span></span>
                 <span>Alice Renucci, Lucie Juneau</span>
             </div>
-            <div className="personnel__heading">Communications and Promotion</div>
+            <div className="personnel__heading">{t("heading-comms-promotion")}</div>
             <div className="personnel__row">
-                <span>Branding and Design</span>
+                <span>{t("roles.branding")}</span>
                 <span></span>
                 <span>Vina Goh</span>
             </div>
             <div className="personnel__row">
-                <span> Web Developer </span>
+                <span>{t("roles.web-dev")}</span>
                 <span></span>
                 <span>Valentin Tsatskin</span>
             </div>
             <div className="personnel__row">
-                <span>Community Manager and Translation (Spanish)</span>
+                <span>{t("roles.community-translation-es")}</span>
                 <span></span>
                 <span>Julia Sahagún</span>
             </div>
             <div className="personnel__row">
-                <span>Translation (French)</span>
+                <span>{t("roles.comms-translation-fr")}</span>
                 <span></span>
                 <span>Tiah Coxon, Alice Renucci</span>
             </div>
             <div className="personnel__row">
-                <span>Videographer, editor, photographer</span>
+                <span>{t("roles.comms-videographer")}</span>
                 <span></span>
                 <span>Juan David Padilla Vega</span>
             </div>
             <div className="personnel__row">
-                <span>Photographer</span>
+                <span>{t("roles.comms-photographer")}</span>
                 <span></span>
                 <span>Denis Farley</span>
             </div>
-            <div className="personnel__heading">Support</div>
+            <div className="personnel__heading">{t("heading-support")}</div>
             <div className="personnel__row">Canada Council for the Arts</div>
             <div className="personnel__row">Le Conseil des arts et des lettres du Québec</div>
             <div className="personnel__row">Goethe Institut-Montréal</div>
             <div className="personnel__row">La Société des arts et des compositeurs dramatiques (SACD)</div>
             <div className="personnel__row">Théâtre Hector-Charland</div>
             <div className="personnel__row">
-                Gerard would like to express their deep gratitude for all the
-                interviewees in Montreal, Berlin and Zürich. Thank you to my sister, Denise Reyes, Tyrone Harper, Julie Favreau and David J. Romero for guiding me as a novice interviewer.
+                {t("text-gratitude")}
             </div>
-            <div className="personnel__heading">Residencies</div>
+            <div className="personnel__heading">{t("heading-residencies")}</div>
             <div className="personnel__row">
                 Fabrik Potsdam, Tanzfabrik Berlin, Tanzhaus Zürich, Grand Studio (Bruxelles), Espace Marie Chouinard, Par B.L.eux/Benoît Lachambre, Compagnie Flak/José Navas, Montréal arts interculturels, Département de danse de l'UQAM, Musée d'art de Joliette
             </div>
-            <div className="personnel__heading">INTERNATIONAL DISTRIBUTION</div>
+            <div className="personnel__heading">{t("heading-international-distro")}</div>
             <div className="personnel__row">
-                PLEASE CONTACT
+                {t("text-contact")}
                 <span>
                     <a className="inline-link" href="mailto:info@planbhamburg.com">PLAN B</a>
                 </span>
