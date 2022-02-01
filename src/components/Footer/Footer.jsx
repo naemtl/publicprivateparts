9584b448a67a488558f76817fd1508cfb94535e3
Matthew Stinis <mpstinis@gmail.com>
2022-01-31 20:09:04 -0500

    add json locales en for footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,19 +1,22 @@
+import { useTranslation } from "react-i18next";
 import { FaYoutube, FaVimeoV, FaFacebookF, FaLinkedinIn } from "react-icons/fa";
 
 import './Footer.scss'
 
 const Footer = ({ backgroundColor }) => {
+    const { t } = useTranslation("footer")
+
     return (
         <div className={`footer ${backgroundColor ? backgroundColor : ''}`}>
             <div className="footer__column">
                 <div>
-                    <div className="footer__label">Contact Gerard</div>
+                    <div className="footer__label">{t("text-contact")} Gerard</div>
                     <a className="footer__anchor--text" href="mailto:gerardxreyes@gmail.com">gerardxreyes@gmail.com</a>
                 </div>
             </div>
             <div className="footer__column footer__column--socials">
                 <div>
-                    <div className="footer__label">Follow Gerard</div>
+                    <div className="footer__label">{t("text-follow")} Gerard</div>
                     <a className="footer__anchor--text" href="https://twitter.com/gerardxreyes">@gerardxreyes</a>
                 </div>
                 <div className="footer__socials">
@@ -33,7 +36,7 @@ const Footer = ({ backgroundColor }) => {
             </div>
             <div className="footer__column">
                 <div>
-                    <div className="footer__label">Follow Public / Private Parts</div>
+                    <div className="footer__label">{t("text-follow")} Public / Private Parts</div>
                     <a className="footer__anchor--text" href="/">@publicprivateparts</a>
                 </div>
             </div>
