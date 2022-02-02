a99725b6df1a6efdd91c14200265e1b0b58fa055
Matthew Stinis <mpstinis@gmail.com>
2022-02-02 14:53:10 -0500

    fix interviews preview videos + add youtube component

--- src/pages/Homepage/InterviewsPreview/InterviewsPreview.jsx
+++ src/pages/Homepage/InterviewsPreview/InterviewsPreview.jsx
@@ -1,42 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import YoutubeEmbed from "../../../components/YoutubeEmbed/YoutubeEmbed"
+
 import './InterviewsPreview.scss'
 
 const InterviewsPreview = ({ t }) => {
     return (
         <div className='interviews-preview'>
+
+            <div className="interviews-preview__title">
+                {t('interviews.title')}
+            </div>
+            <div className="interviews-preview__text">
+                {t('interviews.desc')}
+            </div>
             <div className="interviews-preview__videos">
-                <div className="interviews-preview__videos-wrapper">
-                    <div className="interviews-preview__title">
-                        {t('interviews.title')}
+                <div className="interviews-preview__videos-column">
+                    <YoutubeEmbed embedId="9rH27gpU_40" />
+                    <div className="interviews-preview__video-name">
+                        Mareen
                     </div>
-                    <div className="interviews-preview__text">
-                        {t('interviews.desc')}
+                </div>
+                <div className="interviews-preview__videos-column">
+                    {/* <iframe src="https://www.youtube-nocookie.com/embed/IIBsRHuYAq4" title="YouTube video player"
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen className="interviews-preview__video-player">
+                    </iframe> */}
+                    <YoutubeEmbed embedId="IIBsRHuYAq4" />
+                    <div className="interviews-preview__video-name">
+                        Bishop
                     </div>
-                    <div className="interviews-preview__videos">
-                        <div className="interviews-preview__videos-col">
-                            <iframe src="https://www.youtube-nocookie.com/embed/9rH27gpU_40" title="YouTube video player"
-                                frameBorder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                allowFullScreen className="interviews-preview__video-player">
-                            </iframe>
-                            <div className="interviews-preview__text">Mareen</div>
-                        </div>
-                        <div className="interviews-preview__videos-col">
-                            <iframe src="https://www.youtube-nocookie.com/embed/IIBsRHuYAq4" title="YouTube video player"
-                                frameBorder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                allowFullScreen className="interviews-preview__video-player">
-                            </iframe>
-                            <div className="interviews-preview__text">Bishop</div>
-                        </div>
-                    </div>
-                    <Link className='interviews-preview__button' to="/interviews" >
-                        {t('interviews.anchor-1')}
-                    </Link>
                 </div>
             </div>
+            <Link className='interviews-preview__button' to="/interviews" >
+                {t('interviews.anchor-1')}
+            </Link>
         </div>);
 };
 
