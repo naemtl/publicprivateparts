45873ba3adc30fab38fe79309dda5439d9cf4ce9
Matthew Stinis <mpstinis@gmail.com>
2022-02-02 21:56:09 -0500

    add odm new version, styles + markup

--- src/pages/OrigineDuMonde/OrigineDuMonde.jsx
+++ src/pages/OrigineDuMonde/OrigineDuMonde.jsx
@@ -3,6 +3,13 @@ import { Trans, useTranslation } from "react-i18next";
 import OriginePersonnel from "./OriginePersonnel/OriginePersonnel";
 import Footer from '../../components/Footer/Footer';
 
+import odmImg1 from "../../assets/images/odm-1.png";
+import odmImg2 from "../../assets/images/odm-2.png";
+import odmImg3 from "../../assets/images/odm-3.jpg";
+import odmImg4 from "../../assets/images/odm-4.png";
+import odmImg5 from "../../assets/images/odm-5.png";
+import odmImg6 from "../../assets/images/odm-6.png";
+import odmImg7 from "../../assets/images/odm-7.png";
 import calqLogo from "../../assets/svg/calq-logo.svg";
 import ccaLogo from "../../assets/svg/cca-logo.svg";
 import goetheLogo from "../../assets/svg/goethe-logo.svg";
@@ -31,8 +38,17 @@ const OrigineDuMonde = () => {
                 </video>
             </div>
             <div className="origine__content">
+                <div className="origine__media-row origine__media-row--single">
+                    <div className="origine__video-column">
+                        <YoutubeVimeoEmbed isVimeo={true} embedId="542702988" />
+                    </div>
+                </div>
                 <div className="origine__text">
                     <Trans t={t} i18nKey={"main.paragraph-main-1"} />
+                </div>
+                <div className="origine__media-row">
+                    <img src={odmImg1} alt="Origine Du Monde promo" />
+                    <img src={odmImg2} alt="Origine Du Monde promo" />
                 </div>
                 <div className="origine__text">
                     <Trans t={t} i18nKey={"main.paragraph-main-2"} />
@@ -40,23 +56,17 @@ const OrigineDuMonde = () => {
                 <div className="origine__text">
                     {t("main.text-premiere")}
                 </div>
-                <div className="origine__controls">
-                    <YoutubeVimeoEmbed isVimeo={true} embedId="542702988" />
-                    <a className="button" href="https://vimeo.com/542702988">
-                        {t("main.button-trailer")}
-                        <span> 1</span>
-                    </a>
-                    <a className="button" href="https://vimeo.com/542702988">
-                        {t("main.button-trailer")}
-                        <span> 2</span>
-                    </a>
-                    <a className="button" href="https://vimeo.com/542702988">
-                        {t("main.button-trailer")}
-                        <span> 3</span>
-                    </a>
+                <img src={odmImg3} alt="Origine Du Monde promo" />
+
+                <div className="origine__media-row">
+                    <img src={odmImg4} alt="Origine Du Monde promo" />
+                    <img src={odmImg5} alt="Origine Du Monde promo" />
+                </div>
+                <div className="origine__media-row">
+                    <img src={odmImg6} alt="Origine Du Monde promo" />
+                    <img src={odmImg7} alt="Origine Du Monde promo" />
                 </div>
             </div>
-            <div className="origine__embed"></div>
             <div className="origine__quote">
                 <div className="origine__quote-container">
                     <div className="origine__quote-text">
@@ -67,22 +77,29 @@ const OrigineDuMonde = () => {
                     </div>
                 </div>
             </div>
-            <OriginePersonnel t={t} />
+            <div className="origine__media-row">
+                <div className="origine__video-column">
+                    <YoutubeVimeoEmbed isVimeo={true} embedId="542702988" />
+                </div>
+                <div className="origine__video-column">
+                    <YoutubeVimeoEmbed isVimeo={true} embedId="542702988" />
+                </div>
+            </div>
+            {/* <OriginePersonnel t={t} /> */}
             <div className="origine__logos-container">
                 <div className="origine__logos-row">
                     <div className="origine__personnel-heading">
                         {t("main.heading-1")}
                     </div>
-                    <img src={calqLogo}
-                        alt="Conseil des arts et des lettres du Québec" />
-                    <img src={ccaLogo} className="cca-logo" alt="Canada Council for the Arts" />
-                    <img src={sacdLogo} className="sacd-logo" alt="SACD Canada" />
+                    <img src={calqLogo} className="origine__logo--calq" alt="Conseil des arts et des lettres du Québec" />
+                    <img src={ccaLogo} className="origine__logo--cca" alt="Canada Council for the Arts" />
+                    <img src={sacdLogo} className="origine__logo--sacd" alt="SACD Canada" />
                 </div>
                 <div className="origine__logos-row">
                     <div className="origine__personnel-heading">
                         {t("main.heading-2")}
                     </div>
-                    <img src={goetheLogo} className="goethe-logo" alt="Goethe-Institut" />
+                    <img src={goetheLogo} className="origine__logo--goethe" alt="Goethe-Institut" />
                 </div>
             </div>
             <div className="origine__personnel-row">
