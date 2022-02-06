81976b992557faf55ccd20f1bbc9d1df820e5d06
Matthew Stinis <mpstinis@gmail.com>
2022-02-06 16:25:57 -0500

    fix about page styles markup

--- src/pages/About/ContactBanner/ContactBanner.jsx
+++ src/pages/About/ContactBanner/ContactBanner.jsx
@@ -1,27 +1,31 @@
 import './ContactBanner.scss'
 
+import { FaYoutube, FaVimeoV, FaFacebookF, FaLinkedinIn, FaInstagram } from "react-icons/fa";
+
 const ContactBanner = ({ t }) => {
     return (
         <div className="contact-banner">
-            <div className="contact-banner__inner">
-                <div className="contact-banner__title">{t("text-contact")}</div>
-                <div className="contact-banner__email">
-                    <a href="mailto:gerardxreyes@gmail.com">gerardxreyes@gmail.com</a>
-                </div>
-                <div className="contact-banner__links">
-                    <div style={{ textAlign: "left" }}> <a href="https://vimeo.com/gerardxreyes">Vimeo</a>
-                        <br />
-                        <a href="https://www.youtube.com/channel/UCuOTqqnX4obf7HefTmQZyng">YouTube</a>
-                    </div>
-                    <div>
-                        <a href="https://www.facebook.com/GerardReyesArtist/">Facebook</a>
-                    </div>
-                    <div style={{ textAlign: "right" }}>
-                        <a href="https://instagram.com/gerardxreyes">Instagram</a>
-                        <br />
-                        <a href="https://www.linkedin.com/in/gerard-x-reyes-77952a120">LinkedIn</a>
-                    </div>
-                </div>
+            <div className="contact-banner__title">{t("text-contact")}</div>
+            <div className="contact-banner__email">
+                <a href="mailto:gerardxreyes@gmail.com">gerardxreyes@gmail.com</a>
+            </div>
+            <div className="contact-banner__links">
+                <a href="https://vimeo.com/gerardxreyes">
+                    <FaVimeoV />
+                </a>
+                <a href="https://www.youtube.com/channel/UCuOTqqnX4obf7HefTmQZyng">
+                    <FaYoutube />
+                </a>
+
+                <a href="https://www.facebook.com/GerardReyesArtist/">
+                    <FaFacebookF />
+                </a>
+                <a href="https://instagram.com/gerardxreyes">
+                    <FaInstagram />
+                </a>
+                <a href="https://www.linkedin.com/in/gerard-x-reyes-77952a120">
+                    <FaLinkedinIn />
+                </a>
             </div>
         </div>
     );
