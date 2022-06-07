dd5cf585668f8f6c5ed57b3747dd38e0c8c31da6
stinima1 <matthew.stinis@onespan.com>
2022-06-07 00:19:18 -0400

    change about styles and structure

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,15 +3,12 @@ import { Link } from 'react-router-dom';
 
 import ContactBanner from './ContactBanner/ContactBanner';
 
-import gerardProfileImg from "../../assets/images/gerard-profile.jpg";
-import gerardOneImg from "../../assets/images/gerard-1.jpg";
-import gerardTwoImg from "../../assets/images/gerard-2.jpg";
-import gerardThreeImg from "../../assets/images/gerard-3.jpg";
-import gerardFourImg from "../../assets/images/gerard-4.jpg";
-import gerardFiveImg from "../../assets/images/gerard-5.jpg";
-import gerardSixImg from "../../assets/images/gerard-6.jpg";
-import gerardSevenImg from "../../assets/images/gerard-7.jpg";
-import gerardEightImg from "../../assets/images/gerard-8.jpg";
+import gerardProfileImg from "../../assets/images/gerard-profile.png";
+import gerardOneImg from "../../assets/images/gerard-1.png";
+import gerardTwoImg from "../../assets/images/gerard-2.png";
+import gerardThreeImg from "../../assets/images/gerard-3.png";
+import gerardFourImg from "../../assets/images/gerard-4.png";
+import gerardFiveImg from "../../assets/images/gerard-5.png";
 
 import './About.scss'
 
@@ -20,21 +17,16 @@ const About = () => {
 
     return (
         <div className="about">
+            <div className="about__header">
+                <img className="about__image--profile" alt='Gerard' src={gerardProfileImg} />
+                <h1 className="about__heading">
+                    {t("heading-title")}
+                    <span> Gerard X Reyes</span>
+                </h1>
+            </div>
             <div className="about__content">
-
-                <div className="about__row">
-                    <div className="about__row--text">
-                        <h1 className="about__heading">
-                            {t("heading-title")}
-                            <span> Gerard X Reyes</span>
-                        </h1>
-                        <p>
-                            <Trans t={t} i18nKey={"paragraph-intro"} />
-                        </p>
-                    </div>
-                    <div>
-                        <img className="about__image--profile" alt='Gerard' src={gerardProfileImg} />
-                    </div>
+                <div className="about__row--intro">
+                    <Trans t={t} i18nKey={"paragraph-intro"} />
                 </div>
                 <div className="about__row">
                     <div className="about__row--text">
@@ -46,32 +38,36 @@ const About = () => {
                     </div>
                 </div>
                 <div className="about__row">
-                    <div className="about__row--text">
-                        <Trans t={t} i18nKey={"paragraph-pop"} />
-                    </div>
-                    <div className="about__image-overlap">
-                        <img className="about__image--three" alt="Gerard" src={gerardThreeImg} />
-                        <img className="about__image--four" alt="Gerard" src={gerardFourImg} />
-                    </div>
+                    <img className="about__image--four" alt="Gerard" src={gerardFourImg} />
                 </div>
-                <div className="about__row">
-                    <div className="about__row--text">
-                        <p><Trans t={t} i18nKey={"paragraph-ballroom"} /></p>
-                        <p>{t("paragraph-bodywork")}</p>
-                    </div>
-                    <div>
-                        <img className="about__image--five" alt="Gerard" src={gerardFiveImg} />
-                    </div>
+                <div className="about__row">                        
+                    <Trans t={t} i18nKey={"paragraph-pop"} />
                 </div>
-                <div className="about__row">
+                <div className="about__image-row">
+                    <img className="about__image--three" alt="Gerard" src={gerardThreeImg} />
+                    <img className="about__image--five" alt="Gerard" src={gerardFiveImg} />
+                </div>
+                <div className="about__row--ballroom">
+                    <p>
+                        <Trans t={t} i18nKey={"paragraph-ballroom"} />
+                    </p>
+                    <p>{t("paragraph-bodywork")}</p>
+                </div>
+                <div className="about__row--pop">
                     <div className="about__row--text">
                         <div>
                             <strong><em>{t("text-pop-1")}</em></strong>
                         </div>
                         <div> – <em>{t("text-pop-2")}</em></div>
+                    </div>
+                </div>
+                <div className="about__row--discovery">
+                    <div className="about__row--text">
                         <p>
                             {t("text-discovery-1")}
                         </p>
+                    </div>
+                    <div className="about__row--text">
                         <p>
                             {t("text-discovery-2")}
                             <div className="about__link-container">
@@ -83,15 +79,6 @@ const About = () => {
                                 </a>
                             </div>
                         </p>
-                    </div>
-                    <div className="about__image-overlap">
-                        <img className="about__image--six" alt="Gerard" src={gerardSixImg} />
-                        <img className="about__image--seven" alt="Gerard" src={gerardSevenImg} />
-                    </div>
-                </div>
-                <div className="about__row">
-                    <div>
-                        <img className="about__image--eight" alt="Gerard" src={gerardEightImg} />
                     </div>
                 </div>
             </div>
