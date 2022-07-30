87a6d63f392b61d998be0b0bb1ddbe8d2d3b77ed
Matthew Stinis <mpstinis@gmail.com>
2022-07-30 01:19:33 -0400

    remove loader page spinner and logo

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,14 +1,8 @@
-import { FaSpinner } from "react-icons/fa";
-
-import pppLogo from "../../assets/images/ppp-logo.png";
-
 import "./Loader.scss";
 
 const Loader = () => {
     return (
         <div className='loader'>
-            <img src={pppLogo} alt="Public Private Parts logo" />
-            <FaSpinner />
         </div>
     );
 };
