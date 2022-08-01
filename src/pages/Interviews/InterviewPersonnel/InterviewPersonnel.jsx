744f0c13ccd39412fa0496255f8b8570cfa1f6dc
Matthew Stinis <matthew.stinis@onespan.com>
2022-08-01 01:47:36 -0400

    change interviews page styles

--- src/pages/Interviews/InterviewPersonnel/InterviewPersonnel.jsx
+++ src/pages/Interviews/InterviewPersonnel/InterviewPersonnel.jsx
@@ -3,49 +3,49 @@ import "../../../assets/styles/personnel.scss"
 const InterviewPersonnel = ({ t }) => {
     return (
         <div className="personnel">
-            <h2 className="personnel__heading">{t("main.heading-production")}</h2>
+            <h2 className="personnel__heading"><em>{t("main.heading-production")}</em></h2>
             <div className="personnel__row">
-                <span>{t("prod-roles.prod-director")}</span>
+                <span className="personnel__role--interview">{t("prod-roles.prod-director")}</span>
                 <span></span>
                 <span>Gerard X Reyes</span>
             </div>
             <div className="personnel__row">
-                <span>{t("prod-roles.camera-op")}</span>
+                <span className="personnel__role--interview">{t("prod-roles.camera-op")}</span>
                 <span></span>
                 <span>David J. Romero, Julie Favreau</span>
             </div>
             <div className="personnel__row">
-                <span>{t("prod-roles.editor-drama")}</span>
+                <span className="personnel__role--interview">{t("prod-roles.editor-drama")}</span>
                 <span></span>
                 <span>Julie Favreau</span>
             </div>
             <div className="personnel__row">
-                <span>{t("prod-roles.colour")}</span>
+                <span className="personnel__role--interview">{t("prod-roles.colour")}</span>
                 <span></span>
                 <span>Max Hilsamer</span>
             </div>
             <div className="personnel__row">
-                <span>{t("prod-roles.sound")}</span>
+                <span className="personnel__role--interview">{t("prod-roles.sound")}</span>
                 <span></span>
                 <span>Devon Bate</span>
             </div>
             <div className="personnel__row">
-                <span>{t("prod-roles.subtitles")}</span>
+                <span className="personnel__role--interview">{t("prod-roles.subtitles")}</span>
                 <span></span>
                 <span>T &amp; S Coop (Emma Roufs, Ellen Warkentin, Carolina Alfaro Carvalho)</span>
             </div>
             <div className="personnel__row">
-                <span>{t("prod-roles.transcription-en")}</span>
+                <span className="personnel__role--interview">{t("prod-roles.transcription-en")}</span>
                 <span></span>
                 <span>Gerard X Reyes</span>
             </div>
             <div className="personnel__row">
-                <span>{t("prod-roles.translation-fr")}</span>
+                <span className="personnel__role--interview">{t("prod-roles.translation-fr")}</span>
                 <span></span>
                 <span>Ingrid Vallus</span>
             </div>
             <div className="personnel__row">
-                <span>{t("prod-roles.translation-es")}</span>
+                <span className="personnel__role--interview">{t("prod-roles.translation-es")}</span>
                 <span></span>
                 <span>Julia Sahagún</span>
             </div>
