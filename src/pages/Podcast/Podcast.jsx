e8c5845d5c8a2c12be9ed54d5334017af8cce5a2
Matthew Stinis <matthew.stinis@onespan.com>
2023-04-30 19:40:28 -0400

    add podcast page styles

--- src/pages/Podcast/Podcast.jsx
+++ src/pages/Podcast/Podcast.jsx
@@ -10,43 +10,43 @@ const Podcast = () => {
 
   return (
     <div className="podcast">
-      <div className="podcast__banner"></div>
-      <div className="podcast__intro-container">
-        <div className="podcast__intro-heading">
-          {t("heading")}
-        </div>
-        <div className="podcast__intro-text">
-          {t("intro")}
-        </div>
-      </div>
       <div className="podcast__content">
-        <div className="podcast__episode-container">
-          <div className="podcast__episode-player"></div>
-          <div className="podcast__episode-heading">
-            {t("episode-1.heading")}
-          </div>
-          <div className="podcast__episode-text">
-            {t("episode-1.text")}
-          </div>
-        </div>
-        <div className="podcast__episode-container">
-          <div className="podcast__episode-player"></div>
-          <div className="podcast__episode-heading">
-            {t("episode-2.heading")}
-          </div>
-          <div className="podcast__episode-text">
-            {t("episode-2.text")}
+        <div className="podcast__banner"></div>
+        <div className="podcast__intro-container">
+          <h1 className="podcast__intro-heading">
+            {t("heading")}
+          </h1>
+          <div className="podcast__intro-text">
+            {t("intro")}
           </div>
         </div>
-        <div className="podcast__episode-container">
-          <div className="podcast__episode-player"></div>
-          <div className="podcast__episode-heading">
-            {t("episode-3.heading")}
+          <div className="podcast__episode-container">
+            <div className="podcast__episode-player"></div>
+            <h3 className="podcast__episode-heading">
+              {t("episode-1.heading")}
+            </h3>
+            <div className="podcast__episode-text">
+              {t("episode-1.text")}
+            </div>
           </div>
-          <div className="podcast__episode-text">
-            {t("episode-3.text")}
+          <div className="podcast__episode-container">
+            <div className="podcast__episode-player"></div>
+            <h3 className="podcast__episode-heading">
+              {t("episode-2.heading")}
+            </h3>
+            <div className="podcast__episode-text">
+              {t("episode-2.text")}
+            </div>
           </div>
-        </div>
+          <div className="podcast__episode-container">
+            <div className="podcast__episode-player"></div>
+            <h3 className="podcast__episode-heading">
+              {t("episode-3.heading")}
+            </h3>
+            <div className="podcast__episode-text">
+              {t("episode-3.text")}
+            </div>
+          </div>
       </div>
       <Footer />
     </div>
