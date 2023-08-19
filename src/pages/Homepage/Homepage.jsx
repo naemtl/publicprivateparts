6bde0363d19a2ead9dc6ce9dd67302325a3feab8
Matthew Stinis <matthew.stinis@onespan.com>
2023-08-19 15:03:58 -0400

    add parallax styles and jsx to homepage

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -14,108 +14,37 @@ import "./Homepage.scss";
 
 const Homepage = () => {
   return (
-    // <div className="homepage">
-    //   <MouseParallaxContainer globalFactorX={0.1} globalFactorY={0.1}>
-    //     <MouseParallaxChild factorX={0.3} factorY={0.5}>
-    //       <img src={gxrLeft1} alt="" />
-    //     </MouseParallaxChild>
-    //     <MouseParallaxChild factorX={0.3} factorY={0.5}>
-    //       <img src={gxrLeft1} alt="" />
-    //     </MouseParallaxChild>
-    //     <MouseParallaxChild factorX={0.3} factorY={0.5}>
-    //       <img src={gxrLeft1} alt="" />
-    //     </MouseParallaxChild>
-    //     {/* <img
-    //       className="homepage__background"
-    //       src={gxrLeft2}
-    //       alt="GXR homepage background"
-    //     /> */}
-    //     <MouseParallaxChild factorX={0.7} factorY={0.8}>
-    //       <img src={gxrRight1} alt="" />
-    //     </MouseParallaxChild>
-    //   </MouseParallaxContainer>
-    // </div>
     <div className="homepage">
-      <div
-        style={{
-          height: "100%",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          width: "100%",
-        }}
+      <MouseParallaxContainer
+        className="homepage__parallax-container"
+        globalFactorX={0.3}
+        globalFactorY={0.3}
+        resetOnLeave
       >
-        <MouseParallaxContainer
-          className="parallax"
-          containerStyle={{
-            width: "100%",
-            display: "grid",
-            gridTemplateColumns: "auto auto auto auto",
-            zIndex: 2,
-          }}
-          globalFactorX={0.3}
-          globalFactorY={0.3}
-          resetOnLeave
+        <MouseParallaxChild
+          className="homepage__parallax-child"
+          factorX={0.5}
+          factorY={0.5}
         >
-          <MouseParallaxChild
-            factorX={0.5}
-            factorY={0.5}
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              width: "auto",
-              height: "100%",
-            }}
-          >
-            <img height="50%" src={gxrLeft1} alt="" />
-          </MouseParallaxChild>
-          <MouseParallaxChild
-            factorX={0.7}
-            factorY={0.5}
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              width: "auto",
-              height: "100%",
-            }}
-          >
-            <img height="50%" src={gxrLeft2} alt="" />
-          </MouseParallaxChild>
-          <MouseParallaxChild
-            factorX={0.9}
-            factorY={0.5}
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              width: "auto",
-              height: "100%",
-            }}
-          >
-            <img height="50%" src={gxrLeft3} alt="" />
-          </MouseParallaxChild>
-          <MouseParallaxChild
-            factorX={0.7}
-            factorY={0.5}
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              width: "auto",
-              height: "100%",
-            }}
-          >
-            <img height="50%" src={gxrRight1} alt="" />
-          </MouseParallaxChild>
-        </MouseParallaxContainer>
-        <img
-          className="homepage__background"
-          src={gxrDummy}
-          alt="GXR homepage background"
-        />
-      </div>
+          <div className="homepage__left-group">
+            <img className="homepage__img-1" src={gxrLeft1} alt="" />
+            <img className="homepage__img-2" src={gxrLeft2} alt="" />
+            <img className="homepage__img-3" src={gxrLeft3} alt="" />
+          </div>
+        </MouseParallaxChild>
+        <MouseParallaxChild
+          className="homepage__parallax-child"
+          factorX={0.7}
+          factorY={0.5}
+        >
+          <img className="homepage__img-4" src={gxrRight1} alt="" />
+        </MouseParallaxChild>
+      </MouseParallaxContainer>
+      <img
+        className="homepage__background"
+        src={gxrDummy}
+        alt="GXR homepage background"
+      />
     </div>
   );
 };
