635904f69cf436a57219b785142b6b38ad03ef6d
Matthew Stinis <matthew.stinis@onespan.com>
2023-09-03 03:10:03 -0400

    update homepage styles  + jsx

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -2,11 +2,15 @@ import {
   MouseParallaxContainer,
   MouseParallaxChild,
 } from "react-parallax-mouse";
+import { Link } from "react-router-dom";
 
 import gxrBackground from "../../assets/images/gxr-background.jpg";
 import gxrLeft1 from "../../assets/images/gxr-left-1.png";
 import gxrLeft2 from "../../assets/images/gxr-left-2.png";
 import gxrLeft3 from "../../assets/images/gxr-left-3.png";
+import gxrLogo from "../../assets/images/gxr-logo.png";
+import gxrPop from "../../assets/images/gxr-pop.png";
+import gxrPpp from "../../assets/images/gxr-ppp.png";
 import gxrRight1 from "../../assets/images/gxr-right-1.png";
 
 import "./Homepage.scss";
@@ -26,25 +30,47 @@ const Homepage = () => {
             factorX={0.3}
             factorY={0.3}
           >
-            <div className="homepage__left-group">
-              <img className="homepage__img-1" src={gxrLeft1} alt="" />
-              <img className="homepage__img-2" src={gxrLeft2} alt="" />
-              <img className="homepage__img-3" src={gxrLeft3} alt="" />
-            </div>
+            <Link to="/publicprivateparts">
+              <div className="homepage__left-group">
+                <img className="homepage__img-1" src={gxrLeft1} alt="" />
+                <div>
+                  <img className="homepage__img-2" src={gxrLeft2} alt="" />
+                  {/* <img className="homepage__logo--ppp" src={gxrPpp} alt="" /> */}
+                </div>
+                <img className="homepage__img-3" src={gxrLeft3} alt="" />
+              </div>
+            </Link>
           </MouseParallaxChild>
           <MouseParallaxChild
             className="homepage__parallax-child"
             factorX={0.3}
             factorY={0.3}
           >
-            <img className="homepage__img-4" src={gxrRight1} alt="" />
+            <Link to="/principleofpleasure">
+              <div>
+                <img className="homepage__img-4" src={gxrRight1} alt="" />
+                {/* <img className="homepage__logo--pop" src={gxrPop} alt="" /> */}
+              </div>
+            </Link>
           </MouseParallaxChild>
         </MouseParallaxContainer>
-        <img
-          className="homepage__background"
-          src={gxrBackground}
-          alt="GXR homepage background"
-        />
+        <div className="homepage__mobile-links">
+          <Link to="/publicprivateparts">
+            <img className="homepage__logo--mobile" src={gxrPpp} alt="" />
+          </Link>
+          <Link to="/principleofpleasure">
+            <img className="homepage__logo--mobile" src={gxrPop} alt="" />
+          </Link>
+        </div>
+
+        <div className="homepage__background-container">
+          <img
+            className="homepage__background"
+            src={gxrBackground}
+            alt="GXR homepage background"
+          />
+          <img className="homepage__logo" src={gxrLogo} alt="" />
+        </div>
       </div>
     </div>
   );
