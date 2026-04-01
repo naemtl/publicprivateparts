f64a605790e02721e04496389d84309675c56ecc
Matthew Stinis <mpstinis@gmail.com>
2026-04-01 18:10:55 -0400

    change odm personnel text

--- src/pages/OrigineDuMonde/OriginePersonnel/OriginePersonnel.jsx
+++ src/pages/OrigineDuMonde/OriginePersonnel/OriginePersonnel.jsx
@@ -6,12 +6,36 @@ const OriginePersonnel = ({ t }) => {
   return (
     <div className="origine-personnel">
       <h3 className="origine-personnel__heading personnel__heading--main">
-        <em>{t("personnel.main-heading")}</em>
+        <p>
+          <em>{t("personnel.main-heading-1")}</em>
+        </p>
+        <p>
+          <em>{t("personnel.main-heading-2")}</em>
+        </p>
+        <p><em>Gerard X Reyes</em></p>
       </h3>
       <h3 className="origine-personnel__heading">
         <em>{t("personnel.coprod-heading-1")}</em>
-        <div>{t("personnel.coprod-heading-2")}</div>
       </h3>
+      <h3 className="origine-personnel__heading">
+        <em>{t("personnel.live-performers-heading")}</em>
+      </h3>
+      <div className="origine-personnel__text">
+        <div>Pina Brutal</div>
+        <div>Nica Rosés</div>
+        <div>Gerard X Reyes</div>
+      </div>
+      <h3 className="origine-personnel__heading">
+        <em>{t("personnel.video-performers-heading")}</em>
+      </h3>
+      <div className="origine-personnel__text">
+        <div>Bishop Black</div>
+        <div>Jasko Fide</div>
+        <div>Jorgetheobscene</div>
+        <div>Mara Morgen</div>
+        <div>Rebecca Frances</div>
+        <div>Gerard X Reyes</div>
+      </div>
       <h3 className="origine-personnel__heading">
         <em>{t("personnel.live-performance-heading")}</em>
       </h3>
@@ -19,9 +43,9 @@ const OriginePersonnel = ({ t }) => {
         {t("personnel.live-performance-text")}
       </div>
       <h3 className="origine-personnel__heading">
-        <em>{t("personnel.video-heading")}</em>
+        <em>{t("personnel.video-installation-heading")}</em>
       </h3>
-      <div className="origine-personnel__text">{t("personnel.video-text")}</div>
+      <div className="origine-personnel__text">{t("personnel.video-installation-text")}</div>
       <h3 className="origine-personnel__heading">
         <em>{t("personnel.comms-promotion-heading")}</em>
       </h3>
@@ -29,10 +53,16 @@ const OriginePersonnel = ({ t }) => {
         {t("personnel.comms-promotion-text")}
       </div>
       <h3 className="origine-personnel__heading">
-        <em>{t("personnel.residencies-heading")}</em>
+        <em>{t("personnel.world-premier-heading")}</em>
       </h3>
       <div className="origine-personnel__text">
-        {t("personnel.residencies-text")}
+        Usine C, Montréal
+      </div>
+      <h3 className="origine-personnel__heading">
+        <em>{t("personnel.research-heading")}</em>
+      </h3>
+      <div className="origine-personnel__text">
+        Festival Transamériques, Montréal
       </div>
       <h3 className="origine-personnel__heading">
         <em>{t("personnel.support-heading")}</em>
@@ -41,6 +71,12 @@ const OriginePersonnel = ({ t }) => {
         {t("personnel.support-text")}
       </div>
       <h3 className="origine-personnel__heading">
+        <em>{t("personnel.residencies-heading")}</em>
+      </h3>
+      <div className="origine-personnel__text">
+        {t("personnel.residencies-text")}
+      </div>
+      {/* <h3 className="origine-personnel__heading">
         <em>{t("personnel.partners-heading")}</em>
       </h3>
       <div className="origine-personnel__text">
@@ -63,7 +99,7 @@ const OriginePersonnel = ({ t }) => {
           </a>{" "}
           (HAMBURG, GERMANY) - <em>CREATIVE AGENCY FOR PERFORMING ARTS</em>
         </Trans>
-      </div>
+      </div> */}
     </div>
   );
 };
