84d3c0562af57b68aa1325b4533e59b7a4953b20
gerardxreyes <gerardxreyes@gmail.com>
2026-06-11 16:44:01 -0400

    change bodywork button and text

--- src/pages/Bodywork/Bodywork.jsx
+++ src/pages/Bodywork/Bodywork.jsx
@@ -1,6 +1,4 @@
-import { useState } from "react";
 import { Trans, useTranslation } from "react-i18next";
-import { CopyToClipboard } from "react-copy-to-clipboard";
 
 import Footer from "../../components/Footer/Footer";
 import PageBanner from "../../components/PageBanner/PageBanner";
@@ -11,15 +9,6 @@ import "./Bodywork.scss";
 
 const Bodywork = () => {
   const { t } = useTranslation("bodywork");
-
-  const [isCopied, setIsCopied] = useState(false);
-
-  const onCopyText = () => {
-    setIsCopied(true);
-    setTimeout(() => {
-      setIsCopied(false);
-    }, 1000);
-  };
 
   return (
     <div className="bodywork">
@@ -85,34 +74,16 @@ const Bodywork = () => {
         </ol>
         <div className="bodywork__message">
           <h2 className="bodywork__heading">
-            <em>{t("heading-message")}</em>
+            <em>{t("heading-about")}</em>
           </h2>
+          <p className="bodywork__text">{t("paragraph-about-intro")}</p>
           <p className="bodywork__text">
-            <Trans t={t} i18nKey={"paragraph-message-1"} />
+            <Trans t={t} i18nKey={"paragraph-about-1"} />
           </p>
           <p className="bodywork__text">
-            <Trans t={t} i18nKey={"paragraph-message-2"} />
+            <Trans t={t} i18nKey={"paragraph-about-2"} />
           </p>
-          <p className="bodywork__text">{t("paragraph-message-3")}</p>
-        </div>
-        <p className="bodywork__text--accommodate">
-          <em>{t("paragraph-pricing-2")}</em>
-        </p>
-        <div className="bodywork__copy-clipboard">
-          <CopyToClipboard text={"gerardxreyes@gmail.com"} onCopy={onCopyText}>
-            <span className="bodywork__text--contact">
-              {t("anchor-contact")}
-            </span>
-          </CopyToClipboard>
-          <div
-            className={`${
-              isCopied
-                ? "bodywork__text-copied--show"
-                : "bodywork__text-copied--hide"
-            }`}
-          >
-            {t("text-copied")}!
-          </div>
+          <p className="bodywork__text">{t("paragraph-about-3")}</p>
         </div>
         <div className="bodywork__button--consult">
           <a
@@ -123,13 +94,6 @@ const Bodywork = () => {
             {t("anchor-book")}
           </a>
         </div>
-        <p className="bodywork__text">{t("paragraph-covid")}</p>
-        <p className="bodywork__text">
-          <Trans t={t} i18nKey={"paragraph-thanks"} />
-        </p>
-        <p className="bodywork__text">
-          <em>&copy; 2014 ACSB. {t("paragraph-copyright")}</em>
-        </p>
       </div>
       <Footer />
     </div>
